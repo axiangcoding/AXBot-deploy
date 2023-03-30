c2fa662a8f8528f580ea57e418cb3b68f74880f9
axiangcoding <axiangcoding@gmail.com>
2023-03-30 23:01:36 +0800

    update for deploy

---
+++ install-nginx-debian.sh
@@ -0,0 +1,5 @@
+# https://www.nginx.com/resources/wiki/start/topics/tutorials/install/
+apt install nginx
+# install certbot for auto cert
+# https://www.nginx.com/blog/using-free-ssltls-certificates-from-lets-encrypt-with-nginx/
+apt install certbot python3-certbot-nginx
